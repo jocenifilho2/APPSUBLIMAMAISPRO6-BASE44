77b0fb22aa234749033447780ba200e1792d660b
jocenifilho2 <jocenifilho2@gmail.com>
2026-07-15 15:25:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import { ModoVisitanteProvider } from '@/lib/ModoVisitanteContext';
@@ -68,7 +68,6 @@ const AuthenticatedApp = () => {
 
         <Route path="/admin" element={<ModuloGate moduloId="administrativo"><AcessoAdministrativo /></ModuloGate>} />
         <Route path="/ecommerce" element={<ModuloGate moduloId="ecommerce"><ErpEcommerce /></ModuloGate>} />
-        <Route path="/separacao" element={<Navigate to="/logistica" replace />} />
         <Route path="/logistica" element={<ModuloGate moduloId="logistica"><ErpLogisticaPage /></ModuloGate>} />
       </Route>
       <Route path="/redefinir-senha" element={<RedefinirSenha />} />

4e383dcefc7bf0c74b2755e6df0c7d892128bace
jocenifilho2 <jocenifilho2@gmail.com>
2026-07-15 22:58:52 -0300

    Delete src/lib/src/lib directory

--- src/lib/src/lib/base44-server-url.js
+++
@@ -1,4 +0,0 @@
-export function getBase44ServerUrl() {
-  const isHostedOnBase44 = typeof window !== 'undefined' && window.location.hostname.endsWith('base44.app');
-  return isHostedOnBase44 ? '' : 'https://base44.app';
-}
